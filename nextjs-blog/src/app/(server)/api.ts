32d24e172efe696c1a36710302d4b41f26b02541
1evanter <rita.oleksenkoo@gmail.com>
2025-02-18 19:27:12 +0200

    add getSongByName func

--- nextjs-blog/src/app/(server)/api.ts
+++ nextjs-blog/src/app/(server)/api.ts
@@ -1,6 +1,17 @@
+import { ERRORS } from '../errors';
 import songs from './db.json'
 import { delayed } from './delay'
 
 export function getAllSongs() {
     return delayed(songs)
+}
+
+export function getSongByName(songName: string) {
+    const song = songs.find(x => x.name === songName);
+
+    if (!song) {
+        throw new Error(ERRORS.NOT_FOUND);
+    }
+
+    return delayed(song);
 }
