40cbb8eab0e8028331294a1273667a180a2d1e78
1evanter <rita.oleksenkoo@gmail.com>
2025-02-18 18:34:56 +0200

    Update api.ts

--- nextjs-blog/src/app/(server)/api.ts
+++ nextjs-blog/src/app/(server)/api.ts
@@ -2,5 +2,5 @@ import songs from './db.json'
 import { delayed } from './delay'
 
 export function getAllSongs() {
-    return delayed(songs, {timeout: 1500, shouldFail: false})
+    return delayed(songs)
 }
